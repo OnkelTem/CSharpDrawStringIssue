6a8c56958a044d03ecf5c8f527ab27196a83ef6f
OnkelTem <aneganov@gmail.com>
2024-05-29 01:39:50 +0300

    Initial commit

---
+++ DrawStringIssue/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawStringIssue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(Brushes.Black, ClientRectangle);
+            Console.WriteLine($"OnPaint: {ClientRectangle}");
+            e.Graphics.DrawString($"{ClientRectangle}", Font, Brushes.White, ClientRectangle);
+        }
+    }
+}
